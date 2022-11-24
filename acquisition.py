79fe6cd77d93ee58960b834ee989de3547feb13e
Luis Obis <luis.antonio.obis@gmail.com>
2022-11-24 15:16:21 +0100

    update acquisition

--- acquisition.py
+++ acquisition.py
@@ -1,24 +1,46 @@
-import time
-import matplotlib.pyplot as plt
+from typing import Union
 import numpy as np
 import uproot
-import awkward as ak
 import os
 from pathlib import Path
+
 import lecroyscope
-
-run = 21
-
-scope = lecroyscope.Scope("192.168.10.5")
-
-print(scope.id)
-
-scope.sample_mode = "Sequence"
-print(f"Sample mode: {scope.sample_mode}")
-
-scope.num_segments = 200
-
 from caenhv import CaenHV
+
+# Run parameters
+drift_gap = 10.0  # mm
+
+# HV power supply parameters
+mesh_voltage = 340.0  # V
+drift_voltages = np.arange(95, 815, 15)
+print(f"Drift voltages: {drift_voltages}")
+
+# Oscilloscope parameters
+scope_ip = "192.168.10.5"
+scope_n_sequences = 8
+scope_num_segments = 2500
+
+# run directory
+runs_dir = Path("runs")
+runs = list(filter(os.path.isdir, map(lambda x: runs_dir / x, os.listdir(runs_dir)))) if runs_dir.exists() else []
+run_numbers = set()
+for run in runs:
+    try:
+        run_numbers.add(int(run.name.split("_")[1]))
+    except Union[ValueError, IndexError]:
+        pass
+
+run_number = max(run_numbers) + 1 if len(run_numbers) > 0 else 1
+run_dir = runs_dir / f"run_{run_number:04d}"
+run_dir.mkdir(parents=True, exist_ok=True)
+
+# Set instruments
+scope = lecroyscope.Scope(scope_ip)
+scope.trigger_mode = "Stopped"
+scope.sample_mode = "Sequence"
+scope.timeout = 10 * 60.
+scope.num_segments = scope_num_segments
+print(f"Scope ID: {scope.id} - OK")
 
 caen = CaenHV()
 module = caen[0]
@@ -28,67 +50,55 @@ hv_drift = module.channel(2)
 hv_mesh.rup = 20.0
 hv_drift.rup = 20.0
 
-run_dir = Path(f"run_{run:03d}")
-os.mkdir(run_dir)
-
-# run parameters
-drift_gap = 10.0  # mm
-mesh_voltage = 340.0  # V
 hv_mesh.vset = mesh_voltage
 
-n_sequences = 100
+print("CAEN HV power supply - OK")
 
-hv_drift.on()
-hv_mesh.on()
+try:
+    hv_drift.on()
+    hv_mesh.on()
 
-# voltages = np.arange(312.5, 800, 50)
-voltages = np.arange(95, 815, 15)
+    for voltage in drift_voltages:
+        print(f"Mesh voltage: {mesh_voltage:0.1f} V, Drift voltage: {voltage:0.1f} V")
 
+        root_filename = run_dir / Path(
+            f"gap_{drift_gap:0.1f}_mesh_{mesh_voltage:0.1f}_drift_{voltage:0.1f}.root"
+        )
+        output_file = uproot.recreate(root_filename)
 
-def _make_tree(file, _trace_group):
-    _n_points = len(_trace_group.time)
-    channels = {f"CH{trace.channel}": ("f4", (_n_points,)) for trace in _trace_group}
-    return file.mktree(
-        "t",
-        {"time": ("f4", (_n_points,)), "drift_voltage": np.float32, "mesh_voltage": np.float32, "drift_gap": np.float32,
-         **channels}, "Drift Velocity Measurement"
-    )
+        hv_drift.vset = voltage
+        hv_mesh.vset = mesh_voltage
 
+        hv_drift.wait_for_vset(timeout=60)
+        hv_mesh.wait_for_vset(timeout=60)
 
-# voltages = np.linspace(50, 850, 81)[14:]
-# voltages = np.concatenate(([200], np.arange(650, 875, 25),))
-print(f"Voltages: {voltages}")
+        tree = None
+        for i in range(scope_n_sequences):
+            print(f"sequence {i + 1} of {scope_n_sequences}")
+            if not scope.acquire():
+                continue
 
-for voltage in voltages:
-    print(f"Mesh voltage: {mesh_voltage:0.1f} V, Drift voltage: {voltage:0.1f} V")
+            trace_group = scope.read(2, 3)
 
-    root_filename = run_dir / Path(
-        f"gap_{drift_gap:0.1f}_mesh_{mesh_voltage:0.1f}_drift_{voltage:0.1f}.root"
-    )
-    output_file = uproot.recreate(root_filename)
+            if tree is None:
+                branches = lecroyscope.writing.root.get_tree_branch_definitions(trace_group, drift_voltage=float,
+                                                                                mesh_voltage=float, drift_gap=float)
+                tree = output_file.mktree(
+                    "t",
+                    branches,
+                    "EventTree",
+                )
 
-    hv_drift.vset = voltage
-    hv_mesh.vset = mesh_voltage
+            data = lecroyscope.writing.root.get_tree_extend_data(
+                trace_group,
+                drift_voltage=voltage,
+                mesh_voltage=mesh_voltage,
+                drift_gap=drift_gap,
+            )
+            tree.extend(data)
 
-    hv_drift.wait_for_vset(timeout=60)
-    hv_mesh.wait_for_vset(timeout=60)
+        output_file.close()
 
-    n_points = None
-    tree = None
-    for i in range(n_sequences):
-        print(f"sequence {i + 1} of {n_sequences}")
-        if not scope.acquire(timeout=60):
-            continue
-
-        trace_group = scope.read(2, 3)
-
-        if tree is None:
-            tree = _make_tree(output_file, trace_group)
-
-        n = len(trace_group[2])
-        data = {f"CH{trace.channel}": trace.y for trace in trace_group}
-        data_config = {"drift_voltage": n * [voltage], "mesh_voltage": n * [mesh_voltage], "drift_gap": n * [drift_gap]}
-        data = {"time": n * [trace_group.time], **data, **data_config}
-        tree.extend(data)
-
-    output_file.close()
+finally:
+    hv_drift.off()
+    hv_mesh.off()
